0afb55ce48e73df55120760b4e431e88f368317d
ManhKim <manhtokim@gmail.com>
2022-05-02 23:45:21 +0700

    make it better

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,4 +1,4 @@
-const parse = require('pg-connectiopostgres://mckqvwijdvpjko:b1ae7c1ea1840786236cb32395e274590a66cb5fe0d3e68cf69902ef9c154038@ec2-52-54-212-232.compute-1.amazonaws.com:5432/ddelctbcdlvq6pn-string').parse;
+const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
     connection: {
